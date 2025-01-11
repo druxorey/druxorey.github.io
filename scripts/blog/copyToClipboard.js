620d6cab1384f2137c3cb7186e1ada1a1ebefe16
Druxorey <druxorey@gmail.com>
2025-01-11 13:50:20 -0400

    style(blog): change color on click and revert after 200ms.

--- scripts/blog/copyToClipboard.js
+++ scripts/blog/copyToClipboard.js
@@ -2,8 +2,16 @@ function copyToClipboard(selector) {
     var element = document.querySelector(selector);
     var range = document.createRange();
     range.selectNode(element);
-    window.getSelection().removeAllRanges(); // Clear current selection
-    window.getSelection().addRange(range); // Select text
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
     document.execCommand('copy');
-    window.getSelection().removeAllRanges(); // Deselect text
+    window.getSelection().removeAllRanges();
+}
+
+function changeButtonColor(button) {
+	const originalColor = button.style.backgroundColor;
+	button.style.backgroundColor = 'var(--color-green-2)';
+	setTimeout(() => {
+		button.style.backgroundColor = originalColor;
+	}, 0200);
 }
